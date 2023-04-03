2b71d897f0cf6423cfc69b598518ceb78414ff5b
Alex Skutberg <alsk0312@storsjogymnasiet.se>
2023-04-03 11:11:51 +0200

    hjälp

---
+++ script.js
@@ -0,0 +1,33 @@
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides[currentIndex].classList.remove('visible');
+  currentIndex = index;
+  slides[currentIndex].classList.add('visible');
+}
+
+function showNextSlide() {
+  const nextIndex = (currentIndex + 1) % slides.length;
+  console.log(nextIndex);
+  if (nextIndex > 3) return;  // nummret ska vara den sista sidan som inte kommer att synas
+  showSlide(nextIndex);
+}
+
+function showPrevSlide() {
+  const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
+  console.log(prevIndex);
+  if (prevIndex > 3) return;  // nummret ska vara den sista sidan som inte kommer att synas
+  showSlide(prevIndex);
+}
+
+function showFirstSlide() {
+  showSlide(0);
+}
+
+
+
+document.getElementById('firstBut').addEventListener('click', showFirstSlide);
+document.getElementById('prevBut').addEventListener('click', showPrevSlide);
+document.getElementById('nextBut').addEventListener('click', showNextSlide);
+
