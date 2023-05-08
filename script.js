87bc62af1ff7a74a7308e0c7b30f4a13e29bd63d
OKLIONAS <126885335+OKLIONAS@users.noreply.github.com>
2023-05-08 08:44:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-const slides = document.querySelectorAll('.slide');
-let currentIndex = 0;
+window.onscroll = function() {myFunction()};
 
-function showSlide(index) {
-  slides[currentIndex].classList.remove('visible');
-  currentIndex = index;
-  slides[currentIndex].classList.add('visible');
+var header = document.getElementById("myHeader");
+var header = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > header) {
+    header.classList.add("header");
+  } else {
+    header.classList.remove("header");
+  }
 }
-
-function showNextSlide() {
-  const nextIndex = (currentIndex + 1) % slides.length;
-  console.log(nextIndex);
-  if (nextIndex > 10) return;  // nummret ska vara den sista sidan som inte kommer att synas
-  showSlide(nextIndex);
-}
-
-function showPrevSlide() {
-  const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
-  console.log(prevIndex);
-  if (prevIndex > 10) return;  // nummret ska vara den sista sidan som inte kommer att synas
-  showSlide(prevIndex);
-}
-
-function showFirstSlide() {
-  showSlide(0);
-}
-
-
-
-document.getElementById('firstBut').addEventListener('click', showFirstSlide);
-document.getElementById('prevBut').addEventListener('click', showPrevSlide);
-document.getElementById('nextBut').addEventListener('click', showNextSlide);
-
